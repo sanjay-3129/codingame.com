571b7cefd3ad4e15e98fdb6d55e76498271686ea
sanjay kumar k p <49227865+sanjay-3129@users.noreply.github.com>
2020-10-20 09:08:28 +0530

    added question image as markdown image

--- algorithms/temperatures/Solution.java
+++ algorithms/temperatures/Solution.java
@@ -1,5 +1,6 @@
-//Question Link: https://www.hackerrank.com/challenges/delete-a-node-from-a-linked-list/problem
-![image](https://user-images.githubusercontent.com/49227865/96536674-381f8300-12b2-11eb-9fc6-448da4ad1fcf.png)
+// Question Link: https://www.hackerrank.com/challenges/delete-a-node-from-a-linked-list/problem
+// ![image](https://user-images.githubusercontent.com/49227865/96536674-381f8300-12b2-11eb-9fc6-448da4ad1fcf.png)
+// https://jbt.github.io/markdown-editor/  -  use this link to know the image, just copy and paste the above line in that.
 /*
 Write a program that prints the temperature closest to 0 among input data. 
 If two numbers are equally close to zero, positive integer has to be considered closest to zero (for instance, if the temperatures are -5 and 5, then display 5).
